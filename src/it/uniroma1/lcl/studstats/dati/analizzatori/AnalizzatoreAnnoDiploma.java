25d24e18c0d98d9b26fb99eedd900d950853fca4
Eduardo <eduardo.rinaldi1998@gmail.com>
2018-05-08 17:55:04 +0200

    fixed AnalizzatoreAnnoDiploma

--- src/it/uniroma1/lcl/studstats/dati/analizzatori/AnalizzatoreAnnoDiploma.java
+++ src/it/uniroma1/lcl/studstats/dati/analizzatori/AnalizzatoreAnnoDiploma.java
@@ -18,14 +18,16 @@ public class AnalizzatoreAnnoDiploma implements Analizzatore
     @Override
     public Rapporto generaRapporto(Collection<Studente> studs)
     {
-        HashMap<String, TreeMap<String, Integer>> mappa = new HashMap<>();
-        TreeMap<String, Integer> mappaInterna = new TreeMap<>();
+        HashMap<String, TreeMap<Integer, Integer>> mappa = new HashMap<>();
+        TreeMap<Integer, Integer> mappaInterna = new TreeMap<>((x,y)->-x.compareTo(y));
+
         for(Studente st: studs)
         {
-            String info = st.getInfo().get("ANNO_DIPLOMA");
+            int info = Integer.parseInt(st.getInfo().get("ANNO_DIPLOMA"));
             if(mappaInterna.containsKey(info)) mappaInterna.merge(info, 1, (x,y)->x+y);
             else mappaInterna.put(info,1);
         }
+
         mappa.put("ANNO_DIPLOMA", mappaInterna);
         return new Rapporto(Rapporto.RapportoSemplice.ANNO_DIPLOMA, mappa);
     }
---
+++ src/it/uniroma1/lcl/studstats/dati/analizzatori/AnalizzatoreIstituti.java
@@ -0,0 +1,18 @@
+package it.uniroma1.lcl.studstats.dati.analizzatori;
+
+import it.uniroma1.lcl.studstats.dati.Analizzatore;
+import it.uniroma1.lcl.studstats.dati.Rapporto;
+import it.uniroma1.lcl.studstats.dati.Studente;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class AnalizzatoreIstituti implements Analizzatore
+{
+    // TODO: 08-May-18 implementare analizzatore degli istituti
+    @Override
+    public Rapporto generaRapporto(Collection<Studente> studs) {
+        return null;
+    }
+}
